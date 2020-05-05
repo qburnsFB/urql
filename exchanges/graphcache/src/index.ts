1a42c88ac0ce3a9d408ac0618bfd944b08f4dcc6
Phil Pluckthun <phil@kitten.sh>
2020-05-05 19:06:54 +0100

    Implement initial offlineExchange

--- exchanges/graphcache/src/index.ts
+++ exchanges/graphcache/src/index.ts
@@ -2,4 +2,5 @@ export * from './types';
 export { query, write, writeOptimistic } from './operations';
 export { Store, noopDataState, reserveLayer } from './store';
 export { cacheExchange } from './cacheExchange';
+export { offlineExchange } from './offlineExchange';
 export { populateExchange } from '@urql/exchange-populate';
