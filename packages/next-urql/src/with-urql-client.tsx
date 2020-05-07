70ebeed4b5bbe4f4aabc24ce08346a2ba94e48fa
Jovi De Croock <decroockjovi@gmail.com>
2020-05-07 14:51:38 +0200

    support serverProps by making withUrql.getInitialProps optional

--- packages/next-urql/src/with-urql-client.tsx
+++ packages/next-urql/src/with-urql-client.tsx
@@ -50,54 +50,58 @@ export function withUrqlClient(
     // Set the displayName to indicate use of withUrqlClient.
     withUrql.displayName = `withUrqlClient(${getDisplayName(AppOrPage)})`;
 
-    withUrql.getInitialProps = async (appOrPageCtx: NextUrqlContext) => {
-      const { AppTree } = appOrPageCtx;
+    // TODO: maybe we should also run this when we are sure a page isn't
+    // only on the client-side (Static-page-optimization)
+    if (AppOrPage.getInitialProps) {
+      withUrql.getInitialProps = async (appOrPageCtx: NextUrqlContext) => {
+        const { AppTree } = appOrPageCtx;
 
-      // Determine if we are wrapping an App component or a Page component.
-      const isApp = !!(appOrPageCtx as AppContext).Component;
-      const ctx = isApp
-        ? (appOrPageCtx as AppContext).ctx
-        : (appOrPageCtx as NextPageContext);
+        // Determine if we are wrapping an App component or a Page component.
+        const isApp = !!(appOrPageCtx as AppContext).Component;
+        const ctx = isApp
+          ? (appOrPageCtx as AppContext).ctx
+          : (appOrPageCtx as NextPageContext);
 
-      const opts =
-        typeof clientConfig === 'function' ? clientConfig(ctx) : clientConfig;
-      const [urqlClient, ssrCache] = initUrqlClient(opts, mergeExchanges);
+        const opts =
+          typeof clientConfig === 'function' ? clientConfig(ctx) : clientConfig;
+        const [urqlClient, ssrCache] = initUrqlClient(opts, mergeExchanges);
 
-      if (urqlClient) {
-        (ctx as NextUrqlContext).urqlClient = urqlClient;
-      }
+        if (urqlClient) {
+          (ctx as NextUrqlContext).urqlClient = urqlClient;
+        }
 
-      // Run the wrapped component's getInitialProps function.
-      let pageProps = {} as any;
-      if (AppOrPage.getInitialProps) {
-        pageProps = await AppOrPage.getInitialProps(appOrPageCtx as any);
-      }
+        // Run the wrapped component's getInitialProps function.
+        let pageProps = {} as any;
+        if (AppOrPage.getInitialProps) {
+          pageProps = await AppOrPage.getInitialProps(appOrPageCtx as any);
+        }
 
-      // Check the window object to determine whether or not we are on the server.
-      // getInitialProps runs on the server for initial render, and on the client for navigation.
-      // We only want to run the prepass step on the server.
-      if (typeof window !== 'undefined') {
-        return { ...pageProps, urqlClient };
-      }
+        // Check the window object to determine whether or not we are on the server.
+        // getInitialProps runs on the server for initial render, and on the client for navigation.
+        // We only want to run the prepass step on the server.
+        if (typeof window !== 'undefined') {
+          return { ...pageProps, urqlClient };
+        }
 
-      const props = { ...pageProps, urqlClient };
-      const appTreeProps = isApp ? props : { pageProps: props };
+        const props = { ...pageProps, urqlClient };
+        const appTreeProps = isApp ? props : { pageProps: props };
 
-      // Run the prepass step on AppTree. This will run all urql queries on the server.
-      await ssrPrepass(<AppTree {...appTreeProps} />);
+        // Run the prepass step on AppTree. This will run all urql queries on the server.
+        await ssrPrepass(<AppTree {...appTreeProps} />);
 
-      // Serialize the urqlClient to null on the client-side.
-      // This ensures we don't share client and server instances of the urqlClient.
-      (urqlClient as any).toJSON = () => {
-        return null;
+        // Serialize the urqlClient to null on the client-side.
+        // This ensures we don't share client and server instances of the urqlClient.
+        (urqlClient as any).toJSON = () => {
+          return null;
+        };
+
+        return {
+          ...pageProps,
+          urqlState: ssrCache ? ssrCache.extractData() : undefined,
+          urqlClient,
+        };
       };
-
-      return {
-        ...pageProps,
-        urqlState: ssrCache ? ssrCache.extractData() : undefined,
-        urqlClient,
-      };
-    };
+    }
 
     return withUrql;
   };
